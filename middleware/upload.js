2240b646dfe695c4d2cb53929a221d3ac6a24005
mahmoudalaa <135064108+MahmoudX8@users.noreply.github.com>
2026-08-03 02:25:03 +0300

    upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,33 +1,37 @@
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-
+const cloudinary = require('cloudinary').v2;
 const uploadDir = 'uploads/';
-if(!fs.existsSync(uploadDir)){
-    fs.mkdirSync(uploadDir, {recursive:true});
-}
-// Configure storage
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir); // Make sure this folder exists
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
+const {CloudinaryStorage} = require('multer-storage-cloudinary');
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-// File filter for images only
-// const fileFilter = (req, file, cb) => {
-//     if (file.mimetype.startsWith('image/')) {
-//         cb(null, true);
-//     } else {
-//         cb(new Error('Only images are allowed'), false);
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'nesty-products',
+        allowed_formats: ['jpg', 'jpeg', 'png', 'webp']
+    }
+})
+// if(!fs.existsSync(uploadDir)){
+//     fs.mkdirSync(uploadDir, {recursive:true});
+// }
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, uploadDir); 
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, Date.now() + path.extname(file.originalname));
 //     }
-// };
+// });
+
 
 const upload = multer({ 
     storage: storage,
-    // fileFilter: fileFilter,
     limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
 });
 
