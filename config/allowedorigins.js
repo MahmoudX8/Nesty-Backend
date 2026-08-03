eb775547f6f22f5cba765bb3198e3580c288c442
mahmoudalaa <135064108+MahmoudX8@users.noreply.github.com>
2026-08-03 19:57:11 +0300

    Update allowedorigins.js

--- config/allowedorigins.js
+++ config/allowedorigins.js
@@ -1,2 +1,2 @@
-const allowedorigins = ['https://nesty-frontend-nwzp.vercel.app','http://localhost:3000','https://api.ipify.org/?format=json','https://cqb2kh8v-3000.uks1.devtunnels.ms/'];
+const allowedorigins = ['https://nesty-nwzp.vercel.app','http://localhost:3000','https://api.ipify.org/?format=json','https://cqb2kh8v-3000.uks1.devtunnels.ms/'];
 module.exports = allowedorigins;
